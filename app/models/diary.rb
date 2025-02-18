314c97e2bab57af135609c732f7b51d0c939eb94
root <root@DESKTOP-LDRUM5G>
2025-02-18 21:46:29 +0900

    コメント機能の実装

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,7 @@
 class Diary < ApplicationRecord
   belongs_to :user
   mount_uploader :diary_image, DiaryImageUploader
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
