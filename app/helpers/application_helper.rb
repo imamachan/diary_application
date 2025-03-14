3c522a03bf16e6081b45317fe59a35e7dd104c51
root <root@DESKTOP-LDRUM5G>
2025-03-15 08:36:41 +0900

    calendar機能実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include SimpleCalendar::CalendarHelper
   def page_title(title = "")
     base_title = "HiBiLog"
     title.present? ? "#{title} | #{base_title}" : base_title
