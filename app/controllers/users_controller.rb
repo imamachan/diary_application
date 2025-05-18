78e29c1fb7714d796df51de0bd57f9b0b72c9194
root <root@DESKTOP-LDRUM5G>
2025-05-18 23:51:36 +0900

    edit:users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to login_path
     else
       @hide_header_footer = true
       flash.now[:danger] = @user.errors.full_messages.join(", ")
